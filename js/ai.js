ad3aca7e4bc602f72abae3d5e65b5c4580c2cb75
Grace-creator <47767333+grace-creator@users.noreply.github.com>
2023-05-22 12:51:23 -0500

    add debounding

--- js/ai.js
+++ js/ai.js
@@ -1,15 +1,20 @@
 jQuery(document).ready(function($) {
-    $('#automated-accessibility-checker-analyze').on('click', function() {
+  let timeout;
   
-      const content = wp.data.select('core/editor').getEditedPostContent();
+  $('#automated-accessibility-checker-analyze').on('click', function() {
+
+    clearTimeout(timeout);
+
+    timeout = setTimeout(() => {
+      const content = $('#automated-accessibility-checker-content').val();
       if (content.length === 0) {
         alert('Please enter some content to analyze.');
         return;
       }
-  
+
       // Use the API key from the config file
       const openAIApiKey = automated_accessibility_checker.openai_api_key;
-  
+
       $.ajax({
         url: 'https://api.openai.com/v1/engines/davinci/completions',
         method: 'POST',
@@ -23,15 +28,13 @@ jQuery(document).ready(function($) {
         }),
         contentType: 'application/json',
         success: function(response) {
-          console.log('Success:', response); // Debugging line
           const suggestions = response.choices.map((choice, index) => `<p><strong>Suggestion ${index + 1}:</strong> ${choice.text.trim()}</p>`).join('');
           $('#automated-accessibility-checker-results').html('<p>Suggestions:</p>' + suggestions);
         },    
         error: function(jqXHR, textStatus, errorThrown) {
-          console.log('Error:', textStatus, errorThrown); // Debugging line
           alert('An error occurred. Please try again.');
         }
       });     
-    });
+    }, 1000);
   });
-  
+});
